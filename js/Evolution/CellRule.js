93390712749e3ca8331880b0e7e11aafd2337634
obluda3 <yoshivert992@gmail.com>
2025-06-11 14:09:06 +0200

    update of cell, idk if it works now

--- js/Evolution/CellRule.js
+++ js/Evolution/CellRule.js
@@ -1,134 +1,182 @@
 // Implémentation de CELL de Gunji et al (2008)
 class CellRule {
+    // 0 - empty (S_0)
+    // 1 - m : cell (S_1_1 -> S_1_m = S_1)
+    // m+1 : skeleton (S_2)
+    // m+2 : bubble (S_3)
+    // m+3 : marked (S_4)
+    // m+4 : food
     constructor(){
-        this.m = 6
-        this.is_stable = false;
-        this.phase = 0;
-        this.stimulus_point = null;
-        this.bubble = null;
-        this.moves = 0;
-        this.marked = new Set();
+        this.m = 9;
         this.s = 3;
-        this.n = 1000;
         this.is_global = true;
+
+        this.develop_done = false;
+        this.k = 0;
+        this.k_max = 15;
     }
     random_choice(arr) {
         return arr[Math.floor(Math.random()*arr.length)];
     }
 
+    random_choice_prob(array, probabilities) {
+        // probably doesn't work well
+        var cumulative_prob = [0];
+        var x = 0;
+        for (const prob of probabilities) {
+            x += prob;
+            cumulative_prob.push(x);
+        }
+        var p = Math.random();
+        for (var i = 0; i < array.length-1; i++) {
+            if (p >= cumulative_prob[i] && p <= cumulative_prob[i+1]) {
+                return array[i];
+            }
+        }
+        return array[array.length-1];
+    }
+
     global_iteration(tiles) {
-        var develop = this.development(tiles);
-        // Implémentation de l'algorithme pour "manger 0"
         
-        // (1)
-        if (this.stimulus_point === null) {
-            // définition d'un nouveau "stimulus point" 
-            var cytoskeleton = [];
-            for (const tile of tiles) 
-                if (tile.prevState == this.m)
-                    cytoskeleton.push(tile);
-            if (cytoskeleton.length == 0)
-                return develop;
-            this.stimulus_point = this.random_choice(cytoskeleton);
-
-            // (2) définition d'une nouvelle bulle
-            var zero = [];
-            var sp = this.stimulus_point;
-            for (var i = 0; i < sp.neighbors.length; i++) 
-                if (tiles[sp.neighbors[i]].prevState == 0)
-                    zero.push(tiles[sp.neighbors[i]]);
+        if (!this.develop_done) {
+            var develop = this.development(tiles);
+            if (develop)
+               return true;
+            this.reform_skeleton(tiles);
+            this.develop_done = true;
+            return true;
+        }
+        
+        if (this.k == 0) {
+            var sp = this.choose_stimulus_point(tiles);
+            if (!sp) {
+                this.k = this.k_max;
+                return true;
+            }
+            sp.state = this.m+2;
             
-            var bubble = this.random_choice(zero);
-
-            // (3)
-            for (const tile of tiles)
-                tile.state = tile.prevState == 1 ? this.m : tile.prevState;
-
-            bubble.state = this.stimulus_point.prevState;
-            this.stimulus_point.state = bubble.prevState;
-            this.bubble = this.stimulus_point;
-
-            this.moves = 0;
+            var bubble = this.random_choice(this.neighbor_state(sp, 0, tiles));
+            bubble.state = this.m+1;
+            for (const tile of tiles) {
+                if (tile.prevState == this.m) 
+                    tile.state = this.m+1;
+            } 
+            this.k += 1;
+            return true;
+        }
+        if (this.k >= this.k_max) {
+            this.reform_skeleton(tiles);
+            this.k = 0;
             return true;
         }
 
-        // (4)
-        var bubble = this.bubble;
-        this.marked.add(bubble);
-
-        // (5)
-        var zero_count = 0;
-        for (var i = 0; i < bubble.neighbors.length; i++) 
-            if (tiles[bubble.neighbors[i]].state == 0)
-                zero_count += 1;
-
-        if (zero_count < this.s && this.moves < this.n) {
-            // (7)
-            var non_marked = []
-            for (var i = 0; i < bubble.neighbors.length; i++)
-                if (!this.marked.has(tiles[bubble.neighbors[i]]))
-                    non_marked.push(tiles[bubble.neighbors[i]]);
-            this.moves += 1;
-
-            if (non_marked.length > 0) {
-                // transportation of the bubble
-                var new_bubble = this.random_choice(non_marked);
-                bubble.state = this.m;
-                new_bubble.state = 0;
-                this.bubble = new_bubble;
-                return true;
+        for (const tile of tiles) {
+            if (tile.prevState == this.m+2) { // bubble
+                var bubble_neighbors = this.neighbor_state(tile, this.m+1, tiles);
+                var s = this.neighbor_state(tile,0, tiles).length;
+                if (bubble_neighbors.length == 0 || s >= this.s)
+                    this.k = this.k_max;
+                else {
+                    tile.state = this.m+3; // marked
+                    this.random_choice(bubble_neighbors).state = this.m+2;
+                }
             }
-        } 
-
-        // (8)
-        this.stimulus_point = null;
-        this.bubble = null;
-        this.marked = new Set(); // pas sûr
-        
-        // reconstruction de l'intérieur (on remet à 1 les états pas au bord)
-        for (var i = 0; i < tiles.length; i++) {
-            var count = 0;
-            var tile = tiles[i];
-            var state = tile.prevState;
-            if (state != this.m)
-                continue;
-            
-            for (var j = 0; j < tile.neighbors.length; j++) 
-                if (tiles[tile.neighbors[j]].prevState == 0)
-                    count += 1;    
-            
-            if (count == 0) {
-                tile.state = 1;
-            }
-                
         }
+        this.k += 1;
         return true;
     }
 
-    
+    reform_skeleton(tiles) {
+        for (const tile of tiles) {
+            if (tile.prevState == 0)
+                continue;
+            if (tile.prevState == this.m + 2) {
+                tile.state = 0;
+                continue;
+            }
+            var count = this.neighbor_state(tile, this.m+1, tiles).length + this.neighbor_state(tile, this.m+3, tiles).length + this.neighbor_state(tile, this.m, tiles).length;
+            if (count == 4)
+                tile.state = this.m;
+            else
+                tile.state = this.m+1;
+        }
+    }
+
+    choose_stimulus_point(tiles) {
+        var skeleton = [];
+        for (const tile of tiles) {
+            if (tile.prevState == this.m+1 && this.neighbor_state(tile, 0, tiles).length > 0)
+                skeleton.push(tile);
+        }
+
+        if (skeleton.length == 0)
+            return null;
+        
+        var distances = [];
+        for (const tile of skeleton) {
+            var bar_x = 0;
+            var bar_y = 0;
+            for (var i = 0; i < tile.bounds.length; i+=2) {
+                bar_x += tile.bounds[i];
+                bar_y += tile.bounds[i+1];
+            }
+
+            bar_x = 2 * bar_x / tile.bounds.length;
+            bar_y = 2 * bar_y / tile.bounds.length;
+
+            distances.push(Math.min(
+                Math.abs(bar_x+20)+Math.abs(bar_y+20), 
+                Math.abs(bar_x-20)+Math.abs(bar_y-20), 
+                Math.abs(bar_x-20)+Math.abs(bar_y+20),
+                Math.abs(bar_x+20)+Math.abs(bar_y-20)
+            ));
+        }
+
+        var d_sum = 0;
+        for (const d of distances)
+            d_sum += 1/d;
+
+        var probabilities = [];
+        for (const d of distances) 
+            probabilities.push(1/(d*d_sum));
+        
+        
+        return this.random_choice_prob(skeleton, probabilities);
+    }
 
     development(tiles) {
         var has_changed = false;
-        for (var i = 0; i < tiles.length; i++) {
-            var count = 0;
-            var tile = tiles[i];
+        for (const tile of tiles) {
             var state = tile.prevState;
-            has_changed |= state < this.m;
-            
-            for (var j = 0; j < tile.neighbors.length; j++) {
-                var neighbor = tiles[tile.neighbors[j]];
-                if (neighbor.prevState == 0) {
-                    if (state < this.m && state > 1) 
-                        neighbor.state = state+1;
-                    
-                    count += 1;
-                }     
-            }
+            has_changed |= state < this.m && state > 0;
 
-            if (state < this.m && state > 1)
+            var maxNeighbor = 0;
+            if (state < this.m && state >= 1) { // S_1_k
                 tile.state = state+1;
+                continue;
+            }
+            else if (state == this.m)
+                continue;
+            for (var j = 0; j < tile.neighbors.length; j++) { // S_0
+                var neighbor = tiles[tile.neighbors[j]];
+                if (neighbor.prevState > maxNeighbor && neighbor.prevState < this.m)
+                    maxNeighbor = neighbor.prevState;
+            }
+            if (maxNeighbor > 0) {
+                tile.state = maxNeighbor+1;
+                has_changed = true;
+            }
         }
         return has_changed;
     }
 
+    // renvoie la liste des voisins de tile ayant l'état state
+    neighbor_state(tile, state, tiles) {
+        var result = [];
+        for (var i = 0; i < tile.neighbors.length; i++) {
+            if (tiles[tile.neighbors[i]].prevState == state)
+                result.push(tiles[tile.neighbors[i]]);
+        }
+        return result;
+    }
 }
